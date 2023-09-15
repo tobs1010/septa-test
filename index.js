8482f9895bf712ce2c873a9a635ffa9bc5cac217
tobs1010 <fagbolatobi@gmail.com>
2023-09-15 19:47:19 +0100

    javascript add

---
+++ index.js
@@ -0,0 +1,65 @@
+// Difference between Var, Let and Const
+// var declarations are globally scoped or function scoped
+// var variables can be updated and re-declared within its scope;  const variables can neither be updated nor re-declared.
+// var variables are initialized with undefined, let and const variables are not initialized.
+// var variables can be declared without being initialized.
+
+// let variables are block scoped.
+// let variables can be updated but not re-declared;
+// let variables can also be declared without being initialized
+
+
+// const variables are block scoped
+// const variables can neither be updated nor re-declared
+// const variables must be initialized during declaration
+
+// JAVASCRPIT DATA TYPES
+// Undefined: undefined is when a value is not assigned. If a variable is declared, but not assigned, then its value is undefined:
+// null: It’s just a special value which represents “nothing”, “empty” or “value unknown”.
+// Symbols: A Symbol is a unique and immutable primitive value.The symbol type is used to create unique identifiers for objects.
+// Objects: Object is a value in memory which is possibly referenced by an identifier.Objects are the only mutable values.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+alert("hello world");
+document.write("Hello septa people")
+// variables used to store data
+var school = "unilag"
+console.log("hi guys,its cool to be learning js")
+
+const website ="google"
+const url ="https://www." + website + ".com"
+console.log(url)
+
+const name1 = "Oluwatobi"
+const loc = "Lagos"
+const University = "Unilorin"
+
+console.log(" My name is " + name1 + ","+ " I live at " + loc +" I Schooled in " + University )
